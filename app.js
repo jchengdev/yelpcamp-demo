611b689a1525c224522b6e93624e65ba404a7c0e
jchengdev <30513739+jchengdev@users.noreply.github.com>
2018-04-26 22:25:32 -0300

    final adjustments

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-//DEVELOPER NOTES FOR SHOWCASE
+//DEVELOPER NOTES PAGE
 app.get("/about", function(req, res) {
     res.render("about");
 });

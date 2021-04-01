4c13b529b96728ae67b9e7ab0fb608360d50d1f2
Jonathan Cheng <7626923-jchengdev@users.noreply.gitlab.com>
2021-04-01 15:59:03 -0300

    fix: local dev execution

--- app.js
+++ app.js
@@ -1,78 +1,91 @@
 require('dotenv').config();
-var express                 = require("express"),
-    app                     = express(),
-    bodyParser              = require("body-parser"),
-    mongoose                = require("mongoose"),
-    methodOverride          = require("method-override"),
-    flash                   = require("connect-flash"),
-    passport                = require("passport"),
-    localStrategy           = require("passport-local"),
-    User                    = require("./models/user");
-    //seedDB                  = require("./seeds");
+var http = require('http'),
+  express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  methodOverride = require('method-override'),
+  flash = require('connect-flash'),
+  passport = require('passport'),
+  localStrategy = require('passport-local'),
+  User = require('./models/user');
+//seedDB                  = require("./seeds");
 
-var campgroundRoutes        = require("./routes/campgrounds"),
-    commentRoutes           = require("./routes/comments"),
-    indexRoutes             = require("./routes/index");
+var campgroundRoutes = require('./routes/campgrounds'),
+  commentRoutes = require('./routes/comments'),
+  indexRoutes = require('./routes/index');
 
 //APP CONFIG
-app.use(express.static(__dirname+"/public"));       //FOR GOOD DIRECTORY ORGANIZATION
-app.use(bodyParser.urlencoded({extended: true}));   //STANDARD PACKAGE CALL
-app.set("view engine", "ejs");                      //FOR IGNORING ".ejs" IN RENDER METHODS
-app.use(methodOverride("_method"));                 //SIMULATES PUT HTTP REQUESTS
-app.use(flash());                                   //FOR FLASH MESSAGES
+app.set('port', process.env.PORT || 8080);
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(methodOverride('_method')); // * simulates HTTP PUT/DELETE requests
+app.use(flash());
 
 //DB CONNECTION
-var url = process.env.DATABASE_URL || "mongodb://localhost/yelpcamp_123";
+var url = process.env.DATABASE_URL || 'mongodb://localhost/yelpcamp_123';
 
-mongoose.Promise = global.Promise;                                                  //FIX FOR PACKAGE DEPRECATION
-mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true, keepAlive: true });                                      
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'));  //ON CONNECTION ERROR
-mongoose.connection.once('open', function() {
-    console.log("MongoDB -> \'yelpcamp\' database connected!");
-    //seedDB();
+mongoose.Promise = global.Promise; //FIX FOR PACKAGE DEPRECATION
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  keepAlive: true,
+});
+mongoose.connection.on(
+  'error',
+  console.error.bind(console, 'connection error:')
+);
+mongoose.connection.once('open', function () {
+  console.log("MongoDB -> 'yelpcamp_123' database connected!");
+  //seedDB();
 });
 
 //MOMENT.JS INCLUSION
-app.locals.moment = require("moment");
+app.locals.moment = require('moment');
 
 //AUTHENTICATION AND SESSION CONFIG
-app.use(require("express-session")({
-    secret: "Anything that we want",
+app.use(
+  require('express-session')({
+    secret: 'Anything that we want',
     resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());                         //FOR PLUGIN
-app.use(passport.session());                            //FOR PLUGIN
-passport.use(new localStrategy(User.authenticate()));   //PLUGIN 'passport-local-mongoose' PATTERN
-passport.serializeUser(User.serializeUser());           //PLUGIN 'passport-local-mongoose' PATTERN
-passport.deserializeUser(User.deserializeUser());       //PLUGIN 'passport-local-mongoose' PATTERN
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate())); // 'passport-local-mongoose' pattern
+passport.serializeUser(User.serializeUser()); // 'passport-local-mongoose' pattern
+passport.deserializeUser(User.deserializeUser()); // 'passport-local-mongoose' pattern
 
 //MIDDLEWARE FOR USER SESSION
-app.use(function(req, res, next){
-    res.locals.currentUser  = req.user;             //DEFINES COMMON USER DATA FOR EJS TEMPLATES
-    res.locals.error        = req.flash("error");   //DEFINES GLOBAL FLASH MESSAGE
-    res.locals.success      = req.flash("success"); //DEFINES GLOBAL FLASH MESSAGE
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
 });
 
 //ROUTES SETUP
-app.use("/",indexRoutes);
-app.use("/campgrounds",campgroundRoutes);
-app.use("/campgrounds/:id/comments",commentRoutes);
+app.use('/', indexRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
 
 //DEVELOPER NOTES PAGE
-app.get("/about", function(req, res) {
-    res.render("about");
+app.get('/about', function (req, res) {
+  res.render('about');
 });
 
 //ROUTE: UNKNOWN PATH REDIRECT
-app.get("*", function(req, res) {
-    res.redirect("/");
+app.get('*', function (req, res) {
+  res.redirect('/');
 });
 
 //SERVER START
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("YelpCamp server started...");
+var server = http.createServer(app);
+server.listen(app.get('port'), function () {
+  console.log(`YelpCamp server started on port ${app.get('port')} ...`);
 });
 
 // quit on ctrl-c when running docker in terminal

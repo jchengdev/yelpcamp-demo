7a04d7eaf37de07dd38e52198b9d745db1b0a18a
Jonathan Cheng <7626923-jchengdev@users.noreply.gitlab.com>
2021-04-01 18:43:49 -0300

    refactor: reverting db image

--- db/mongo_shell.js
+++
@@ -1,5 +0,0 @@
-use yelpcamp_123
-db.dummyCollction.insert({prop:"value"})
-db.getMongo().getDBNames()
-db.createUser({ user: "username", pwd: "password", roles: [ { role: "readWrite", db: "reporting" } ], mechanisms: [ "SCRAM-SHA-1" ] })
-db.getUsers()
